be7dc53c5736fe427ceff12960b7852a92a88940
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2022-01-14 22:19:48 -0300

    Update project

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let div = document.querySelector('.write-div')
 let clas = div.classList
 
-div.addEventListener('click', function () {
-  let result = clas.toggle('write-div')
+function addBackground() {
+  let result = clas.toggle('.active')
   let body = document.querySelector('body')
-
+  let h1 = document.querySelector('h1')
   console.log(result)
+
   if (result == false) {
     body.classList.add('background')
+    div.classList.add('active')
+    h1.classList.add('active')
+  } else {
+    body.classList.remove('background')
+    div.classList.remove('active')
+    h1.classList.remove('active')
   }
-})
+}
+
+div.addEventListener('click', addBackground)
